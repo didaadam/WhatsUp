80163ac7c72756b247e9f8280f5124ced5c813e7
d.arizona <d.arizona@ekad.com>
2021-07-10 00:00:37 +0700

    update config

--- src/Latihan.js
+++ src/Latihan.js
@@ -3,16 +3,8 @@ import { View, Text } from 'react-native'
 
 function Latihan() {
     return (
-        <View style={{ flex: 1, backgroundColor: 'green', paddingTop: 25 }}>
-            <View style={{ height: '50%', backgroundColor: 'pink', flexDirection: 'row', justifyContent: 'space-around' }}>
-                <Text>atas kiri</Text>
-                <Text>atas kanan</Text>
-            </View>
-
-            <View style={{ height: '50%', backgroundColor: 'blue', flexDirection: 'row', justifyContent: 'space-between', alignItems:'flex-end' }}>
-                <Text>bawah kiri</Text>
-                <Text>bawah kanan</Text>
-            </View>
+        <View style={{ flex: 1, paddingTop: 25 }}>
+            
         </View>
     )
 }
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,10 @@
 import React, { useRef } from 'react'
-import { View, Text, TouchableOpacity, Dimensions } from 'react-native'
+import { View, Text, TouchableOpacity, Dimensions, Image } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import FA5 from 'react-native-vector-icons/FontAwesome5'
 import Toast from "react-native-fast-toast";
+import images from './lib/image'
 
 const Height = Dimensions.get('screen').height
 const Width = Dimensions.get('screen').width
@@ -15,45 +16,37 @@ function Login({ navigation, route }) {
         // <View style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
             <Toast ref={toast} />
-            <LinearGradient colors={['#c2e8f2', '#87ddf5', '#87ddf5', '#c2e8f2']} style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#94b8f2', padding: 20, paddingTop: 20 }}>
-                <View style={{ height: '50%', width: '100%', alignItems: 'center', justifyContent: 'flex-end' }}>
-                    <TouchableOpacity onPress={() => navigation.navigate('Latihan')} style={{ height: 100, width: 100, backgroundColor: '#87ddf5', borderRadius: 20, alignItems:'center', justifyContent:'center'}}>
+            <LinearGradient colors={['#c2e8f2', '#F6A9C6', '#F6A9C6', '#c2e8f2']} style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#94b8f2', padding: 20, paddingTop: 20 }}>
+                <View style={{ height: '60%', width: '100%', alignItems: 'center', justifyContent: 'flex-end' }}>
+                    <Image source={images.logoApp} style={{ width: 175, height: 175, borderRadius: 175 }} />
+                    {/* <TouchableOpacity onPress={() => navigation.navigate('Latihan')} style={{ height: 100, width: 100, backgroundColor: '#87ddf5', borderRadius: 20, alignItems:'center', justifyContent:'center'}}>
                         <FA5 name='galactic-senate' size={100}/>
-                    </TouchableOpacity>
+                    </TouchableOpacity> */}
                 </View>
-                <View style={{ height: '50%', width: '100%', alignItems: 'center', paddingTop: 60 }}>
+                <View style={{ height: '40%', width: '100%', alignItems: 'center', paddingTop: 60 }}>
                     <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
-                        Whats Up
+                        Diary Pregnancy
                     </Text>
                     <Text style={{ marginTop: 10, textAlign: 'center', color: '#538491' }}>
-                        Ini adalah karya anak bangsa untuk menemani kegabutan yang sangat tinggi, thanks.
+                        Aplikasi karya mahasiswi Sarjana Kebidanan Angkatan 2019 untuk ibu hamil , Semoga bermanfaat 🤍
                     </Text>
-
-                    <TouchableOpacity onPress={() => toast.current.show(' Belum dibikin euy..', {type: 'success', icon: <FA5 name="grin-beam-sweat" size={25} color={'white'}/>})} style={{ marginTop: 80, borderRadius: 25, paddingHorizontal: 15, paddingVertical: 10, backgroundColor: 'transparent', width: '100%', flexDirection: 'row', borderColor: 'white', borderWidth: .3 }}>
-                        <View style={{ paddingHorizontal: 7, paddingVertical: 7, backgroundColor: '#d60d0d', borderRadius: 50, justifyContent: 'center' }}>
-                            <MaterialCommunityIcons name={'email'} size={20} color={'white'} />
+                    {/* toast.current.show(' Dah dibilang belum dibikin buset!', { type: 'danger', icon: <FA5 name="head-side-virus" size={25} color={'white'} /> }) */}
+                    <TouchableOpacity onPress={() => navigation.navigate('Latihan')} style={{ marginTop: 40, borderRadius: 25, paddingHorizontal: 15, paddingVertical: 10, backgroundColor: '#3b5998', width: '100%', flexDirection: 'row', borderColor: '#3b5998', borderWidth: .3 }}>
+                        <View style={{ justifyContent: 'center', flex: 1, alignItems: 'center'}}>
+                            <Text style={{ fontWeight: '800', color: 'white' }}>Continue</Text>
                         </View>
-                        <View style={{ justifyContent: 'center', flex: 1, alignItems: 'center', marginLeft: -25 }}>
-                            <Text style={{fontWeight: '800'}}>Login with Email</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => toast.current.show(' Dah dibilang belum dibikin buset!', {type: 'danger', icon: <FA5 name="head-side-virus" size={25} color={'white'}/>})} style={{ marginTop: 10, borderRadius: 25, paddingHorizontal: 15, paddingVertical: 10, backgroundColor: '#3b5998', width: '100%', flexDirection: 'row', borderColor: '#3b5998', borderWidth: .3 }}>
                         {/* <View style={{ paddingHorizontal: 7, paddingVertical: 7, backgroundColor: 'transparent', borderRadius: 50, justifyContent: 'center' }}> */}
-                            <FA5 name={'facebook'} size={30} color={'white'} />
+                        <FA5 name={'angle-double-right'} size={30} color={'white'} />
                         {/* </View> */}
-                        <View style={{ justifyContent: 'center', flex: 1, alignItems: 'center', marginLeft: -25 }}>
-                            <Text style={{fontWeight: '800', color: 'white'}}>Login with Facebook</Text>
-                        </View>
                     </TouchableOpacity>
-                    <View style={{marginTop: 10, width: '100%', alignItems: 'center', justifyContent: 'center', flexDirection: 'row'}}>
-                        <Text style={{fontSize: 12}}>Don't have an account?</Text>
-                        <Text onPress={() => navigation.navigate('SignUp')} style={{color: '#5891a1', fontWeight: 'bold', fontSize: 13}}> Signup</Text>
+                    <View style={{ justifyContent: 'flex-end', flex: 1, paddingBottom: 20 }}>
+                        <Text style={{ marginTop: 10, textAlign: 'center', color: '#538491' }}>
+                            - STIKes Abdi Nusantara Jakarta -
+                        </Text>
                     </View>
+
                 </View>
             </LinearGradient>
-            {/* <Text>Details Screen</Text>
-            <TouchableOpacity onPress={() => navigation.push('Details2', { asd: 'das', route })}><Text>press btn</Text></TouchableOpacity> */}
         </View>
     );
 }
